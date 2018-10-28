ca1f71d36f2530018d5a8198fc86842aace7627e
Brazesh Tamang <brazesh@inficare.net>
2018-10-28 15:32:48 +0545

    Added images

--- app/src/main/java/com/inficare/iremit/testapp/MainActivity.kt
+++ app/src/main/java/com/inficare/iremit/testapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.inficare.iremit.testapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.aagito.imageradiobutton.RadioImageGroup
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioImageGroup.setOnCheckedChangeListener(object: RadioImageGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(radioGroup: View, radioButton: View?, isChecked: Boolean, checkedId: Int) {
+
+                println(checkedId)
+
+            }
+
+        })
     }
 }
